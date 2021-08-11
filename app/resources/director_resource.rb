6d9a204b2e9e3a620a48accad6e76227212a4ad6
anonymous anonymous <rafalal@gmail.com>
2021-08-11 05:25:44 +0000

    Generate graphiti resources for directors

---
+++ app/resources/director_resource.rb
@@ -0,0 +1,13 @@
+class DirectorResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :first_name, :string
+  attribute :last_name, :string
+  attribute :director_bio, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
